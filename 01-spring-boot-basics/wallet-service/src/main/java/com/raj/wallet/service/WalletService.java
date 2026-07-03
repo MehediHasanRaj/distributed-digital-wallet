b54d14cf0740bf2ef5efc0af04a4fe622534868c
MEHEDI HASAN RAJ <mehedihasanraj007@gmail.com>
2026-07-03 20:47:04 +0100

    Add structured logging using SLF4J and Logback

--- 01-spring-boot-basics/wallet-service/src/main/java/com/raj/wallet/service/WalletService.java
+++ 01-spring-boot-basics/wallet-service/src/main/java/com/raj/wallet/service/WalletService.java
@@ -3,18 +3,24 @@ package com.raj.wallet.service;
 import com.raj.wallet.dto.request.CreateWalletRequest;
 import com.raj.wallet.dto.response.WalletResponse;
 import com.raj.wallet.exception.WalletNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletService {
 
+    //logger added, why final, as we only need one instance per class
+    private static final Logger logger = LoggerFactory.getLogger(WalletService.class);
+
     public WalletResponse getWallet(Long id) {
         if (id <= 0) {
-
+            logger.warn("Invalid wallet id received: {}", id);
             throw new WalletNotFoundException(
                     "Wallet with id " + id + " not found"
             );
         }
+        logger.info("Wallet {} fetched successfully", id);
         return new WalletResponse(id, "Raj", 5000.00);
 
     }
