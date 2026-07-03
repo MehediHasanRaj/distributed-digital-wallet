531943e0a19a3e8df22e9d4ac959def794c3a4d9
MEHEDI HASAN RAJ <mehedihasanraj007@gmail.com>
2026-07-03 21:27:43 +0100

    Add unit and controller testing examples using JUnit and Mockito

---
+++ 01-spring-boot-basics/wallet-service/src/test/java/com/raj/wallet/WalletServiceTest.java
@@ -0,0 +1,36 @@
+package com.raj.wallet;
+
+import com.raj.wallet.dto.response.WalletResponse;
+import com.raj.wallet.exception.WalletNotFoundException;
+import com.raj.wallet.service.WalletService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletServiceTest {
+
+    private final WalletService walletService = new WalletService();
+
+    @Test
+    void shouldReturnWallet() {
+
+        WalletResponse response = walletService.getWallet(1L);
+
+        assertEquals(1L, response.getId());
+        assertEquals("Raj", response.getOwner());
+
+    }
+
+    @Test
+    void shouldThrowWalletNotFoundException() {
+
+        assertThrows(WalletNotFoundException.class, () -> walletService.getWallet(-1L));
+
+    }
+
+}
+
+mockMvc.perform(
+        get("/api/v1/wallets/1")
+)
+        .andExpect(status().isOk());
