b8574faea5a794f94a2a87063d752d8fe91e6794
MEHEDI HASAN RAJ <mehedihasanraj007@gmail.com>
2026-07-10 21:52:52 +0100

    Initialize wallet service project structure

--- 03-microservices/wallet-service/src/main/resources/db/migration/V1__Create_wallet_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE wallets (
-                         id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                         user_id CHAR(36),
-                         balance DECIMAL(19,2),
-                         currency VARCHAR(10),
-                         status VARCHAR(20),
-                         version BIGINT
-);
