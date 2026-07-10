7fe6a73b78bf97770ec49dc9e6d3670854e25c46
MEHEDI HASAN RAJ <mehedihasanraj007@gmail.com>
2026-07-10 22:20:06 +0100

    Implement wallet persistence layer

--- 03-microservices/wallet-service/src/main/java/com/raj/wallet/WalletServiceApplication.java
+++ 03-microservices/wallet-service/src/main/java/com/raj/wallet/WalletServiceApplication.java
@@ -2,8 +2,10 @@ package com.raj.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WalletServiceApplication {
 
 	public static void main(String[] args) {
